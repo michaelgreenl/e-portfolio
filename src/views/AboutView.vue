c53ee1c3f030e74f5aaa5fee7c483384bbf6f3ae
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-07-03 16:51:04 -0400

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about"></div>
+</template>
+
+<script setup>
+</script>
+
+<style lang='scss' scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script setup>
+</script>
+
+<script lang='scss' scoped></script>
