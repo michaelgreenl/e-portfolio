5d731034ac2504987b5594541aa6cd69b8b3d50d
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-08-22 16:42:37 -0400

    feat: added preset to button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,7 @@
-<template>
-    <button :class="type" @click="onClick()">
-        <component :is="iconLeft"></component>
-        {{ text }}
-        <component :is="iconRight"></component>
-    </button>
-</template>
-
 <script setup>
 defineProps({
-    type: {
-        type: String, // primary, secondary
+    preset: {
+        type: String, // primary, primary-accent, secondary
     },
     onClick: {
         type: Function,
@@ -23,78 +15,147 @@ defineProps({
     iconRight: {
         type: Object,
     },
+    styles: {
+        type: Object,
+    },
 });
 </script>
+
+<template>
+    <button :class="preset" :style="{ ...styles }" @click="onClick()">
+        <component :is="iconLeft" class="icon" />
+        <span>
+            {{ text }}
+        </span>
+        <component :is="iconRight" class="icon" />
+    </button>
+</template>
 
 <style lang="scss" scoped>
 button {
     display: flex;
-    gap: 0.3em;
     align-items: center;
     justify-content: center;
-    padding: $size-2 $size-4;
     font-family: $primary-font-stack;
     font-size: 1em;
     font-weight: 400;
-    letter-spacing: 0.1ch;
     background: transparent;
-    border-radius: 12px;
 
-    svg {
-        height: $size-4;
-    }
+    &.primary {
+        gap: 0.3em;
+        padding: $size-2 $size-4;
+        letter-spacing: 0.1ch;
+        border-radius: 12px;
 
-    @include theme-dark {
-        color: $color-text-primary;
-        border: solid 2px $color-text-primary;
-
-        svg {
-            fill: $color-text-primary;
+        .icon {
+            height: $size-4;
         }
-    }
-
-    @include theme-light {
-        color: $color-primary-darker;
-        border: solid 2px $color-primary-darker;
-
-        svg {
-            fill: $color-primary-darker;
-        }
-    }
-
-    @include interactive {
-        color: $color-bg-primary;
 
         @include theme-dark {
-            background: $color-text-primary;
+            color: $color-text-primary;
+            border: solid 1px $color-text-primary;
 
-            svg {
-                fill: $color-bg-primary;
+            .icon {
+                fill: $color-text-primary;
             }
         }
 
         @include theme-light {
-            background: $color-primary-darker;
+            color: $color-primary-darker;
+            border: solid 2px $color-primary-darker;
 
-            svg {
-                fill: $color-bg-primary;
+            .icon {
+                fill: $color-primary-darker;
             }
         }
-    }
 
-    &:active {
-        transform: scale(0.9);
+        @include interactive {
+            color: $color-bg-primary;
+            transform: scale(1.05);
+
+            @include theme-dark {
+                background: $color-text-primary;
+
+                .icon {
+                    fill: $color-bg-primary;
+                }
+            }
+
+            @include theme-light {
+                background: $color-primary-darker;
+
+                .icon {
+                    fill: $color-bg-primary;
+                }
+            }
+        }
+
+        &:active {
+            transform: scale(0.95);
+        }
+
+        &.primary-accent {
+            color: $color-bg-primary;
+
+            @include theme-dark {
+                background: $color-text-primary;
+            }
+
+            @include theme-light {
+                background: $color-primary-darker;
+            }
+        }
     }
 
     &.secondary {
-        color: $color-bg-primary;
+        position: relative;
+        gap: $size-2;
+        border: 0;
+        padding: 0;
 
         @include theme-dark {
-            background: $color-text-primary;
+            color: $color-text-primary;
         }
 
         @include theme-light {
-            background: $color-primary-darker;
+            color: $color-primary-darker;
+        }
+
+        .icon {
+            height: $size-5;
+            width: $size-5;
+
+            @include theme-dark {
+                fill: lighten-color($color-text-muted, 10%);
+            }
+
+            @include theme-light {
+                fill: $color-primary-darker;
+            }
+        }
+
+        &::after {
+            content: '';
+            position: absolute;
+            bottom: -6px;
+            left: 1.9em;
+            right: 100%;
+            height: 1px;
+            transition: all 0.3s ease;
+
+            @include theme-dark {
+                background-color: $color-text-secondary;
+            }
+
+            @include theme-light {
+                background-color: $color-primary-darker;
+            }
+        }
+
+        @include interactive {
+            &::after {
+                right: 0;
+            }
         }
     }
 }
