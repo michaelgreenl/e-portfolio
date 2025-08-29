2265c925f3717f8b77bc0b390ee0647bd6355aad
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-08-29 14:49:08 -0400

    fix: added properties to the tools object to help with responsiveness

--- src/components/ToolChip.vue
+++ src/components/ToolChip.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { markRaw } from 'vue';
 import { useThemeStore } from '../stores/themeStore.js';
+import { useMediaQuery } from '@vueuse/core';
 import VueIcon from '../components/SVGs/Tools/VueIcon.vue';
 import GitIcon from '../components/SVGs/Tools/GitIcon.vue';
 import NuxtIcon from '../components/SVGs/Tools/NuxtIcon.vue';
@@ -23,20 +24,51 @@ defineProps({
 
 const themeStore = useThemeStore();
 
+const isMobile = useMediaQuery('(min-width: 682px)');
 const tools = {
-    vue: { text: 'Vue', icon: markRaw(VueIcon), lightColor: '#41B883', darkColor: '#41B883' },
-    git: { text: 'Git', icon: markRaw(GitIcon), lightColor: '#DE4C36', darkColor: '#DE4C36' },
-    nuxt: { text: 'Nuxt', icon: markRaw(NuxtIcon), lightColor: '#00AC65', darkColor: '#00DC82' },
-    gsap: { text: 'Gsap', icon: markRaw(GsapIcon), lightColor: '#00AC65', darkColor: '#00DC82' },
-    pinia: { text: 'Pinia', icon: markRaw(PiniaIcon), lightColor: '#CFA900', darkColor: '#FFE56C' },
-    sass: { text: 'Sass', icon: markRaw(SassIcon), lightColor: '#CD6799', darkColor: '#CD6799' },
-    node: { text: 'Node', icon: markRaw(NodeIcon), lightColor: '#78B537', darkColor: '#8CC84B' },
-    socket: { text: 'Socket.io', icon: markRaw(SocketIcon), lightColor: '#111111', darkColor: '#ffffff' },
-    express: { text: 'Express', icon: markRaw(ExpressIcon), lightColor: '#111111', darkColor: '#ffffff' },
-    postgres: { text: 'Postgres', icon: markRaw(PostgresIcon), lightColor: '#336791', darkColor: '#4789BE' },
-    prisma: { text: 'Prisma', icon: markRaw(PrismaIcon), lightColor: '#586C8D', darkColor: '#7588A9' },
-    sequelize: { text: 'Sequelize', icon: markRaw(SequelizeIcon), lightColor: '#027099', darkColor: '#03afef' },
-    mysql: { text: 'MySQL', icon: markRaw(MySQLIcon), lightColor: '#0081A5', darkColor: '#0081A5' },
+    vue: { text: 'Vue', icon: markRaw(VueIcon), lightColor: '#41B883', darkColor: '#41B883', maxWidth: '6.5em' },
+    git: { text: 'Git', icon: markRaw(GitIcon), lightColor: '#DE4C36', darkColor: '#DE4C36', maxWidth: '6.5em' },
+    nuxt: { text: 'Nuxt', icon: markRaw(NuxtIcon), lightColor: '#00AC65', darkColor: '#00DC82', maxWidth: '6.5em' },
+    gsap: { text: 'Gsap', icon: markRaw(GsapIcon), lightColor: '#00AC65', darkColor: '#00DC82', maxWidth: '6.5em' },
+    pinia: { text: 'Pinia', icon: markRaw(PiniaIcon), lightColor: '#CFA900', darkColor: '#FFE56C', maxWidth: '6.5em' },
+    sass: { text: 'Sass', icon: markRaw(SassIcon), lightColor: '#CD6799', darkColor: '#CD6799', maxWidth: '6.5em' },
+    node: { text: 'Node', icon: markRaw(NodeIcon), lightColor: '#78B537', darkColor: '#8CC84B', maxWidth: '6.5em' },
+    socket: {
+        text: 'Socket.io',
+        icon: markRaw(SocketIcon),
+        lightColor: '#111111',
+        darkColor: '#ffffff',
+        maxWidth: '7.5em',
+    },
+    express: {
+        text: 'Express',
+        icon: markRaw(ExpressIcon),
+        lightColor: '#111111',
+        darkColor: '#ffffff',
+        maxWidth: '7em',
+    },
+    postgres: {
+        text: 'Postgres',
+        icon: markRaw(PostgresIcon),
+        lightColor: '#336791',
+        darkColor: '#4789BE',
+        maxWidth: '7.5em',
+    },
+    prisma: {
+        text: 'Prisma',
+        icon: markRaw(PrismaIcon),
+        lightColor: '#586C8D',
+        darkColor: '#7588A9',
+        maxWidth: '6.5em',
+    },
+    sequelize: {
+        text: 'Sequelize',
+        icon: markRaw(SequelizeIcon),
+        lightColor: '#027099',
+        darkColor: '#03afef',
+        maxWidth: '7.5em',
+    },
+    mysql: { text: 'MySQL', icon: markRaw(MySQLIcon), lightColor: '#0081A5', darkColor: '#0081A5', maxWidth: '7em' },
 };
 </script>
 
@@ -46,6 +78,7 @@ const tools = {
         :style="{
             backgroundColor:
                 themeStore.theme === 'light' ? `${tools[tool].lightColor}30` : `${tools[tool].darkColor}30`,
+            maxWidth: isMobile ? tools[tool].maxWidth : '100vw',
         }"
     >
         <div class="icon-container">
