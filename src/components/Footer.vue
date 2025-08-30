0afb33d238840a48d957c771b8377ea9f15dcd62
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-08-29 20:00:53 -0400

    feat: added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer>
+        <p>© 2025 M.G.</p>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 2em;
+    width: 100%;
+    padding-right: $size-8;
+    padding-bottom: 5em !important;
+
+    p {
+        font-family: $primary-font-stack !important;
+        color: $color-text-muted;
+    }
+
+    @include bp-md-tablet {
+        padding-bottom: 0 !important;
+    }
+}
+</style>
