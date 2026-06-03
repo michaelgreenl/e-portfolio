052157ac67a9f28bb266227157c8efd57c00bce3
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-06-03 15:10:28 -0400

    refactor: selected-project redesign

--- src/components/Project/SelectedProject.vue
+++ src/components/Project/SelectedProject.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue';
 import Button from '@/components/Button.vue';
 import ToolChip from '@/components/ToolChip.vue';
 
-import CalendarIcon from '@/components/SVGs/CalendarIcon.vue';
 import CloseIcon from '@/components/SVGs/CloseIcon.vue';
 
 const props = defineProps({
@@ -31,60 +30,68 @@ defineExpose({ el, overlay });
         :aria-label="activeProject.title"
         @keydown.esc="emit('close-project')"
     >
+        <button @click="() => emit('close-project')" class="close-btn">
+            <CloseIcon />
+        </button>
         <div class="selected-project">
-            <div class="selected-header">
-                <div>
-                    <CalendarIcon />
-                    <p>{{ activeProject.longDate }}</p>
-                </div>
+            <div class="project-header">
+                <div class="project-header-info">
+                    <div class="project-title">
+                        <component :is="projectLogos[activeProject.slug]" />
 
-                <Button @click="() => emit('close-project')" preset="secondary" :iconRight="CloseIcon" />
-            </div>
-
-            <div class="selected-body">
-                <div v-if="activeProject.video" class="selected-img-container">
-                    <iframe
-                        :src="`https://player.vimeo.com/video/${activeProject.video}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=${autoplayVideo ? '1' : '0'}&amp;muted=1`"
-                        frameborder="0"
-                        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        title="game-lobby-demo-vid"
-                    ></iframe>
-                </div>
-
-                <div class="selected-info">
-                    <div class="selected-info-header" :class="`info-header-${activeProject.slug}`">
-                        <component :is="projectLogos[activeProject.slug]" class="project-logo" />
                         <h2 :style="{ fontFamily: activeProject.fontFamily }">
                             {{ activeProject.title }}
                         </h2>
-
-                        <div class="external-links external-links-selected">
-                            <template v-for="(link, key) in activeProject.externalLinks" :key="link">
-                                <a
-                                    v-if="key !== 'demoVideo'"
-                                    :class="`${key === 'liveSite' ? 'no-fill' : undefined}`"
-                                    :href="link.href"
-                                    target="_blank"
-                                >
-                                    <Button :text="link.text" :iconLeft="externalIcons[key]" preset="secondary" />
-                                </a>
-                            </template>
-                        </div>
                     </div>
 
-                    <div class="selected-tool-chips" :class="`tool-chips-${activeProject.slug}`">
+                    <p class="description description-short">{{ activeProject.description.short }}</p>
+                </div>
+
+                <div class="tool-container">
+                    <div class="tool-chips" :class="{ 'large-stack': activeProject.stack.length > 5 }">
                         <ToolChip v-for="tool in activeProject.stack" :key="tool" :tool="tool" class="chip" />
                     </div>
+                </div>
+            </div>
 
-                    <ul class="selected-description">
-                        <li v-for="description in activeProject.description?.long" :key="description">
-                            {{ description }}
-                        </li>
-                    </ul>
+            <div class="project-body">
+                <iframe
+                    v-if="activeProject.video"
+                    :src="`https://player.vimeo.com/video/${activeProject.video}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=${autoplayVideo ? '1' : '0'}&amp;muted=1`"
+                    frameborder="0"
+                    allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    :title="activeProject.slug"
+                ></iframe>
+
+                <ul class="description description-long" :class="{ 'contains-video': activeProject.video }">
+                    <li v-for="description in activeProject.description?.long" :key="description">
+                        {{ description }}
+                    </li>
+                </ul>
+            </div>
+
+            <div class="project-footer">
+                <div class="external-links">
+                    <a
+                        v-for="[key, link] in Object.entries(activeProject.externalLinks).filter(
+                            ([key]) => key !== 'demoVideo' && key !== 'porfolioLink',
+                        )"
+                        :key="key"
+                        :class="{ 'no-fill': key === 'liveSite' }"
+                        :href="link.href"
+                        target="_blank"
+                    >
+                        <Button :text="link.text" :iconLeft="externalIcons[key]" preset="secondary" />
+                    </a>
+                </div>
+
+                <div class="date">
+                    <p>{{ activeProject.longDate }}</p>
                 </div>
             </div>
         </div>
+
         <div ref="overlay" class="overlay" @click="emit('close-project')"></div>
     </div>
 </template>
@@ -106,17 +113,62 @@ p {
 }
 
 .selected-container {
+    font-size: 1.1em;
     position: fixed;
     top: 0;
     z-index: 2;
+    @include flexCenterAll;
     width: 100vw;
     height: 100vh;
     backdrop-filter: blur(5px);
-    backdrop-filter: blur(5px);
-    @include flexCenterAll;
 
     @include theme-dark {
         background-color: rgb(0 0 0 / 40%);
+    }
+}
+
+.close-btn {
+    position: absolute !important;
+    z-index: 100;
+    top: $size-3;
+    right: $size-4;
+    font-size: 1.2em;
+    @include flexCenterAll;
+    height: $size-10;
+    width: $size-10;
+    padding: 0.9em;
+    border: 0;
+    outline: 0;
+    border-radius: 100%;
+    background: transparent;
+
+    @include bp-xl-desktop {
+        top: $size-4;
+        right: $size-5;
+    }
+
+    svg {
+        @include theme-dark {
+            fill: $color-gray4 !important;
+        }
+
+        @include theme-light {
+            fill: $color-gray8 !important;
+        }
+    }
+
+    @include interactive {
+        @include theme-dark {
+            background-color: #49505750;
+        }
+
+        @include theme-light {
+            background-color: #49505710;
+        }
+    }
+
+    &::after {
+        display: none !important;
     }
 }
 
@@ -125,21 +177,16 @@ p {
     z-index: 2;
     display: flex;
     flex-direction: column;
-    max-width: 61em;
-    padding: $size-5 $size-8;
-    margin: 0 $size-4 $size-12;
+    gap: $size-2;
+    max-height: 95dvh;
+    overflow-y: auto;
+    width: 95vw;
+    max-width: 94em;
+    padding: 2.4em $size-11;
+    margin: $size-8 0;
     border: 1px solid rgb(255 255 255 / 12%);
     border-radius: 20px;
     box-shadow: 0 8px 32px 0 rgb(0 0 0 / 37%);
-
-    @include bp-custom-min(450) {
-        padding: $size-8 $size-10;
-    }
-
-    @include bp-md-tablet {
-        max-width: 72em;
-        margin: $size-8 0;
-    }
 
     @include theme-dark {
         background: linear-gradient(0deg, #212529ea 30%, #212529aa 60%, #212529ea 90%);
@@ -150,104 +197,26 @@ p {
     }
 }
 
-.selected-header {
+.project-header {
+    display: flex;
+    gap: $size-7;
+}
+
+.project-header-info {
+    display: flex;
+    flex-direction: column !important;
+}
+
+.project-title {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    padding-left: 0.75em;
-
-    div {
-        display: flex;
-        gap: $size-2;
-        align-items: center;
-        font-size: 1.45em;
-
-        svg {
-            height: 1.1em;
-            stroke: $color-text-secondary;
-        }
-    }
-
-    :deep(button) {
-        gap: 0;
-        padding: $size-3;
-        font-size: 1.2em;
-        border-radius: 100%;
-        @include flexCenterAll;
-
-        svg {
-            @include theme-dark {
-                fill: lighten-color($color-text-muted, 15%);
-            }
-
-            @include theme-light {
-                fill: darken-color($color-text-muted, 40%);
-            }
-        }
-
-        @include interactive {
-            @include theme-dark {
-                background-color: #49505730;
-            }
-
-            @include theme-light {
-                background-color: #49505710;
-            }
-        }
-
-        &::after {
-            display: none !important;
-        }
-    }
-}
-
-.selected-img-container {
-    position: relative;
-    @include flexCenterAll;
-
-    margin: $size-2 0;
-
-    @include bp-sm-phone {
-        margin: 0;
-    }
-
-    iframe {
-        width: 90%;
-        max-width: 65em;
-        aspect-ratio: 2/1.1;
-        margin: $size-4 0 $size-8;
-        border: 1px solid $color-bg-secondary;
-        border-radius: 12px;
-        box-shadow: 0 8px 16px 0 rgb(0 0 0 / 37%);
-    }
-}
-
-.selected-info {
-    display: flex;
-    flex-direction: column;
-    margin-top: -$size-3;
-}
-
-.selected-info-header {
-    display: flex;
     gap: $size-2;
-    align-items: center;
-    margin-top: $size-4;
-    font-size: 1.35em;
+    font-size: 1.2em;
 
-    @include bp-xsm-phone {
-        margin-top: 0;
-    }
-
-    &-algo-visualizer {
-        gap: $size-1 !important;
-    }
-
-    .project-logo {
+    svg {
         display: flex;
         align-items: center;
-        height: 2.2em;
-
+        height: clamp(3.7em, 5.5vw, 4em) !important;
         @include theme-dark {
             fill: $color-gray3;
         }
@@ -274,64 +243,50 @@ p {
         }
 
         @include bp-sm-phone {
-            font-size: 2.4em !important;
+            font-size: clamp(3.7em, 5.5vw, 4em) !important;
         }
     }
 }
 
-.selected-tool-chips {
-    @include flexCenterAll;
-
-    flex-wrap: wrap;
-    gap: $size-4;
-    height: $size-10;
-    margin-top: $size-3;
-    overflow: hidden;
-    font-size: 1.1em;
-
-    .chip {
-        flex: 1;
-        height: 1.8em;
-        font-size: 1.2em;
-    }
-
-    &-algo-visualizer {
-        :deep(.chip-container) {
-            max-width: none !important;
-        }
-    }
-}
-
-.selected-description {
+.project-body {
     display: flex;
-    flex-direction: column;
-    gap: $size-2;
-    padding: $size-3 $size-5 $size-2;
-    margin: $size-2 0;
-    font-family: $secondary-font-stack;
-    font-size: 1.5em;
-    color: $color-text-secondary;
+    gap: $size-9;
     border-top: solid 1px $color-text-muted;
+    border-bottom: solid 1px $color-text-muted;
+    padding: $size-8 0;
+    margin: $size-4 0;
+
+    @include bp-lg-laptop {
+        gap: $size-10;
+    }
+}
+
+iframe {
+    width: 45%;
+    aspect-ratio: 2/1.1;
+}
+
+.project-footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 $size-2;
 }
 
 .external-links {
-    &-selected {
-        display: flex;
-        gap: $size-4;
-        margin-left: auto;
-        font-size: 1.1em;
+    display: flex;
+    gap: $size-6;
+    font-size: 1.5em;
 
-        .no-fill {
-            &:deep(button) svg {
-                stroke-width: 0 !important;
+    .no-fill {
+        &:deep(button) svg {
+            stroke-width: 0 !important;
 
-                @include theme-dark {
-                    fill: lighten-color($color-text-muted, 15%) !important;
-                }
+            @include theme-dark {
+                fill: lighten-color($color-text-muted, 15%) !important;
+            }
 
-                @include theme-light {
-                    fill: darken-color($color-text-muted, 15%) !important;
-                }
+            @include theme-light {
+                fill: darken-color($color-text-muted, 15%) !important;
             }
         }
     }
@@ -374,6 +329,66 @@ p {
             @include theme-light {
                 fill: darken-color($color-text-muted, 15%) !important;
             }
+        }
+    }
+}
+
+.tool-container {
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+    justify-content: center;
+    min-width: 46%;
+}
+
+.tool-chips {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: $size-4;
+    font-size: 1.2em;
+    justify-content: center;
+    padding-right: $size-3;
+
+    .chip {
+        flex: 1;
+        font-size: 1.2em;
+    }
+
+    &.large-stack {
+        font-size: 1.1em;
+    }
+}
+
+.date {
+    display: flex;
+    gap: $size-2;
+    align-items: center;
+    white-space: nowrap;
+    font-size: 1.3em;
+}
+
+.description {
+    font-family: $secondary-font-stack;
+    color: $color-text-secondary;
+    font-size: 1.5em;
+
+    &-short {
+        margin: $size-1 0 0;
+    }
+
+    &-long {
+        flex: 1.2;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: $size-2;
+        margin: 0;
+        line-height: 2ch;
+
+        &.contains-video {
+            font-size: clamp(1.3em, 1.7vw, 1.4em);
+            padding: 0;
         }
     }
 }
