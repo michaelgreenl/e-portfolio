1fc56cf978d388c7284e3a399e2b836827c9fd02
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-08-25 16:08:43 -0400

    fix: svg icon clipping

--- src/components/SVGs/Tools/IonicIcon.vue
+++ src/components/SVGs/Tools/IonicIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-1 -1 102 102">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 -2 100 104">
         <path
             fill="#3880ff"
             fill-rule="evenodd"
